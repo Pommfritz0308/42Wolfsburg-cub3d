a62e68da13ee727b4e97eb228e3ac320e906b7d2
wrottger <wrottger@student.42wolfsburg.de>
2023-11-29 10:12:15 +0100

    fix: change mlx to linux

--- src/parser_map.c
+++ src/parser_map.c
@@ -5,22 +5,22 @@
 
 // }
 
-char	**read_map(t_params *data)
-{
-	char	**map;
-	char	*l;
+// char	**read_map(t_params *data)
+// {
+// 	char	**map;
+// 	char	*l;
 
-	l = gnl_w_counter(data, data->map_fd);
-	while (l && )
-	{
-		if (l[0] && l[0] != '\n')
-		{
-			free(l);
-			l = gnl_w_counter(data, data->map_fd);
-			continue ;
-		}
-		map[i] = gnl_w_counter(data, data->map_fd);
-	}
-	close(data->map_fd);
-	return (NULL);
-}
+// 	l = gnl_w_counter(data, data->map_fd);
+// 	while (l && )
+// 	{
+// 		if (l[0] && l[0] != '\n')
+// 		{
+// 			free(l);
+// 			l = gnl_w_counter(data, data->map_fd);
+// 			continue ;
+// 		}
+// 		map[i] = gnl_w_counter(data, data->map_fd);
+// 	}
+// 	close(data->map_fd);
+// 	return (NULL);
+// }
