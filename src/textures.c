5af70eed7f087dce439bf608ea8df0d30c6e7a84
wrottger <wrottger@student.42wolfsburg.de>
2023-12-11 16:47:42 +0100

    change texture based on orientation

--- src/textures.c
+++ src/textures.c
@@ -10,7 +10,20 @@ void	find_hit(t_params *data, double perp_wall_dist, t_point ray_dir)
 	else
 		g->wall_x = g->pos.x + perp_wall_dist * ray_dir.x;
 	g->wall_x -= floor(g->wall_x);
+}
 
+static int	get_tex_num(int side, t_point direction)
+{
+	if (side == 0 && direction.x > 0)
+	{
+        return 1;
+    } else if (side == 1 && direction.y < 0){
+        return 3;
+    } else if (side == 0 && direction.x < 0) {
+        return 0;
+    } else {
+        return 2;
+	}
 }
 
 void	fill_texture_buffer(t_params *data, int x, t_point ray_dir)
@@ -19,21 +32,23 @@ void	fill_texture_buffer(t_params *data, int x, t_point ray_dir)
 	double		tex_pos;
 	u_int32_t	color;
 	int			y;
+	int			tex_num;
 
-	data->game.tex.x = (int)(data->game.wall_x * (double)data->textures[0].width);
+	tex_num = get_tex_num(data->game.side, ray_dir);
+	data->game.tex.x = (int)(data->game.wall_x * (double)data->textures[tex_num].width);
 	if (data->game.side == 0 && ray_dir.x > 0)
-		data->game.tex.x = (double)data->textures[0].width - data->game.tex.x - 1;
+		data->game.tex.x = (double)data->textures[tex_num].width - data->game.tex.x - 1;
 	if (data->game.side == 1 && ray_dir.y < 0)
-		data->game.tex.x = (double)data->textures[0].width - data->game.tex.x - 1;
-	step = 1.0 * data->textures[0].height / data->game.line_height;
+		data->game.tex.x = (double)data->textures[tex_num].width - data->game.tex.x - 1;
+	step = 1.0 * data->textures[tex_num].height / data->game.line_height;
 	tex_pos = (data->game.draw_start - WINDOW_HEIGHT
 			/ 2 + data->game.line_height / 2) * step;
 	y = data->game.draw_start;
 	while (y++ < data->game.draw_end)
 	{
-		data->game.tex.y = (int)tex_pos & (data->textures[0].height - 1);
+		data->game.tex.y = (int)tex_pos & (data->textures[tex_num].height - 1);
 		tex_pos += step;
-		color = my_mlx_get_pixel_color(data->textures[0],
+		color = my_mlx_get_pixel_color(data->textures[tex_num],
 				data->game.tex.x, data->game.tex.y);
 		my_mlx_pixel_put(data->image, x, y, color);
 	}
